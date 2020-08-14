64653040bd980aa1cfde86dd1152b6b0e6eb4133
Mas7erMind <info.crypto.franco@gmail.com>
2020-08-14 10:56:16 -0400

    Refactor namespaces #2

---
+++ app/src/main/java/com/scala/vault/util/RestoreHeight.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2018 m2049r
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.scala.vault.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+public class RestoreHeight {
+    static private RestoreHeight Singleton = null;
+
+    static public RestoreHeight getInstance() {
+        if (Singleton == null) {
+            synchronized (RestoreHeight.class) {
+                if (Singleton == null) {
+                    Singleton = new RestoreHeight();
+                }
+            }
+        }
+        return Singleton;
+    }
+
+    private Map<String, Long> blockheight = new HashMap<>();
+
+    RestoreHeight() {
+        blockheight.put("2020-08-01", 0L);
+        blockheight.put("2020-08-08", 4500L);
+    }
+
+    public long getHeight(String date) {
+        SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
+        parser.setTimeZone(TimeZone.getTimeZone("UTC"));
+        parser.setLenient(false);
+        try {
+            return getHeight(parser.parse(date));
+        } catch (ParseException ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+
+    public long getHeight(final Date date) {
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        cal.set(Calendar.DST_OFFSET, 0);
+        cal.setTime(date);
+        cal.add(Calendar.DAY_OF_MONTH, -4); // give it some leeway
+        if (cal.get(Calendar.YEAR) < 2014)
+            return 0;
+        if ((cal.get(Calendar.YEAR) == 2014) && (cal.get(Calendar.MONTH) <= 3))
+            // before May 2014
+            return 0;
+
+        Calendar query = (Calendar) cal.clone();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        String queryDate = formatter.format(date);
+
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+        long prevTime = cal.getTimeInMillis();
+        String prevDate = formatter.format(prevTime);
+        // lookup blockheight at first of the month
+        Long prevBc = blockheight.get(prevDate);
+        if (prevBc == null) {
+            // if too recent, go back in time and find latest one we have
+            while (prevBc == null) {
+                cal.add(Calendar.MONTH, -1);
+                if (cal.get(Calendar.YEAR) < 2014) {
+                    throw new IllegalStateException("endless loop looking for blockheight");
+                }
+                prevTime = cal.getTimeInMillis();
+                prevDate = formatter.format(prevTime);
+                prevBc = blockheight.get(prevDate);
+            }
+        }
+        long height = prevBc;
+        // now we have a blockheight & a date ON or BEFORE the restore date requested
+        if (queryDate.equals(prevDate)) return height;
+        // see if we have a blockheight after this date
+        cal.add(Calendar.MONTH, 1);
+        long nextTime = cal.getTimeInMillis();
+        String nextDate = formatter.format(nextTime);
+        Long nextBc = blockheight.get(nextDate);
+        if (nextBc != null) { // we have a range - interpolate the blockheight we are looking for
+            long diff = nextBc - prevBc;
+            long diffDays = TimeUnit.DAYS.convert(nextTime - prevTime, TimeUnit.MILLISECONDS);
+            long days = TimeUnit.DAYS.convert(query.getTimeInMillis() - prevTime,
+                    TimeUnit.MILLISECONDS);
+            height = Math.round(prevBc + diff * (1.0 * days / diffDays));
+        } else {
+            long days = TimeUnit.DAYS.convert(query.getTimeInMillis() - prevTime,
+                    TimeUnit.MILLISECONDS);
+            height = Math.round(prevBc + 1.0 * days * (24 * 60 / 2));
+        }
+        return height;
+    }
+}
---
+++ app/src/main/java/com/scala/vault/xlato/network/XlaToApiImpl.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2017 m2049r et al.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.scala.vault.xlato.network;
+
+import android.support.annotation.NonNull;
+
+import com.scala.vault.util.OkHttpHelper;
+import com.scala.vault.xlato.XlaToError;
+import com.scala.vault.xlato.XlaToException;
+import com.scala.vault.xlato.api.CreateOrder;
+import com.scala.vault.xlato.api.QueryOrderParameters;
+import com.scala.vault.xlato.api.QueryOrderStatus;
+import com.scala.vault.xlato.api.XlaToApi;
+import com.scala.vault.xlato.api.XlaToCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import timber.log.Timber;
+
+public class XlaToApiImpl implements XlaToApi, XlaToApiCall {
+
+    @NonNull
+    private final OkHttpClient okHttpClient;
+
+    private final HttpUrl baseUrl;
+
+    public XlaToApiImpl(@NonNull final OkHttpClient okHttpClient, final HttpUrl baseUrl) {
+        this.okHttpClient = okHttpClient;
+        this.baseUrl = baseUrl;
+    }
+
+    @Override
+    public void createOrder(final double amount, @NonNull final String address,
+                            @NonNull final XlaToCallback<CreateOrder> callback) {
+        CreateOrderImpl.call(this, amount, address, callback);
+    }
+
+    @Override
+    public void createOrder(@NonNull final String url,
+                            @NonNull final XlaToCallback<CreateOrder> callback) {
+        CreateOrderImpl.call(this, url, callback);
+    }
+
+    @Override
+    public void queryOrderStatus(@NonNull final String uuid,
+                                 @NonNull final XlaToCallback<QueryOrderStatus> callback) {
+        QueryOrderStatusImpl.call(this, uuid, callback);
+    }
+
+    @Override
+    public void queryOrderParameters(@NonNull final XlaToCallback<QueryOrderParameters> callback) {
+        QueryOrderParametersImpl.call(this, callback);
+    }
+
+
+    @Override
+    public void call(@NonNull final String path, @NonNull final NetworkCallback callback) {
+        call(path, null, callback);
+    }
+
+    @Override
+    public void call(@NonNull final String path, final JSONObject request, @NonNull final NetworkCallback callback) {
+        final HttpUrl url = baseUrl.newBuilder()
+                .addPathSegment(path)
+                .addPathSegment("") // xmr.to needs a trailing slash!
+                .build();
+
+        Timber.d(url.toString());
+        final Request httpRequest = createHttpRequest(request, url);
+        Timber.d(httpRequest.toString());
+        Timber.d(request == null ? "null request" : request.toString());
+
+        okHttpClient.newCall(httpRequest).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(final Call call, final IOException ex) {
+                Timber.d("A");
+                callback.onError(ex);
+            }
+
+            @Override
+            public void onResponse(final Call call, final Response response) throws IOException {
+                Timber.d("onResponse code=%d", response.code());
+                if (response.isSuccessful()) {
+                    try {
+                        final JSONObject json = new JSONObject(response.body().string());
+                        callback.onSuccess(json);
+                    } catch (JSONException ex) {
+                        callback.onError(ex);
+                    }
+                } else {
+                    try {
+                        final JSONObject json = new JSONObject(response.body().string());
+                        Timber.d(json.toString(2));
+                        final XlaToError error = new XlaToError(json);
+                        Timber.e("xmr.to says %d/%s", response.code(), error.toString());
+                        callback.onError(new XlaToException(response.code(), error));
+                    } catch (JSONException ex) {
+                        callback.onError(new XlaToException(response.code()));
+                    }
+                }
+            }
+        });
+    }
+
+    private Request createHttpRequest(final JSONObject request, final HttpUrl url) {
+        if (request != null) {
+            final RequestBody body = RequestBody.create(
+                    MediaType.parse("application/json"), request.toString());
+            return OkHttpHelper.getPostRequest(url, body);
+        } else {
+            return OkHttpHelper.getGetRequest(url);
+        }
+    }
+}
